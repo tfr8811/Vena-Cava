c9511a44ae8eb92adfc193be8b3371bf6d6724f4
Tom-Roman <69944327+tfr8811@users.noreply.github.com>
2024-06-28 00:46:11 -0400

    Fixed Navmesh and imported level

--- scenes/info/InfoNavRegion.cs
+++ scenes/info/InfoNavRegion.cs
@@ -18,6 +18,9 @@ public partial class InfoNavRegion : NavigationRegion3D, ICsFGD
         NavigationMesh = new NavigationMesh();
         NavigationMesh.GeometrySourceGeometryMode = NavigationMesh.SourceGeometryMode.GroupsWithChildren;
         NavigationMesh.GeometrySourceGroupName = WORLD_GROUP;
+        NavigationMesh.AgentMaxSlope = 20;
+        NavigationMesh.AgentMaxClimb = 0.75f;
+        NavigationMesh.AgentRadius = 0.75f;
         BakeNavigationMesh(false);
     }
 }
